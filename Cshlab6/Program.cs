24810989a64995c0c8fd84711b9662669d6777b0
AHTOH2001 <antonlapytko@gmail.com>
2020-04-26 08:59:44 +0300

    Добавьте файлы проекта.

---
+++ Cshlab6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cshlab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
